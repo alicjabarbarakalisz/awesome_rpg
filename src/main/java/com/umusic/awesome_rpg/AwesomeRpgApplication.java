144d4bed76089453e849c517070a7ccd441106e3
Alicja Kalisz <alicja.kalisz@umusic.com>
2022-05-17 09:59:42 +0300

    first commit

---
+++ src/main/java/com/umusic/awesome_rpg/AwesomeRpgApplication.java
@@ -0,0 +1,13 @@
+package com.umusic.awesome_rpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeRpgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwesomeRpgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/umusic/awesome_rpg/AwesomeRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umusic.awesome_rpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomeRpgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
